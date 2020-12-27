fe4813a1490619323d6455ddbc739bd2bb5b8b56
Tim La <email@example.com>
2020-12-26 20:57:36 -0500

    displaying id in data

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
       <ul>
         {kitchenInfo.map((item) => (
           <div key={item.id}>
+            <li>id: {item.id}</li>
             <li>Room Shape: {item.room_shape}</li>
             <li>Refrigerator Style: {item.refrigerator_style}</li>
             <li>Counter Top: {item.counter_top}</li>
