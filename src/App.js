bd6d36bae6296206d7bfb12cbf538bf6a089b4f8
Tim La <email@example.com>
2020-12-26 20:46:51 -0500

    django backend connected to react frontend

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [kitchenInfo, setKitchenInfo] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://127.0.0.1:8000/api/kitchen/')
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div>
       <h1>Hello World</h1>
