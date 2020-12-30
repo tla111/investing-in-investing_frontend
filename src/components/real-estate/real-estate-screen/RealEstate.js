b49452690c46f8c55513215e561a427fb2e362c5
Tim La <email@example.com>
2020-12-30 07:28:03 -0500

    select form container designed

--- src/components/real-estate/real-estate-screen/RealEstate.js
+++ src/components/real-estate/real-estate-screen/RealEstate.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { Container, Row } from 'react-bootstrap';
+import './realestate.css';
 import KitchenForm from '../forms/KitchenForm';
 // import KitchenData from '../rooms/kitchen/KitchenData';
 
 const RealEstate = () => {
   return (
-    <Container fluid>
-      <Row className='forms_container'>
-        <KitchenForm />
-      </Row>
-      {/* <Row className='room_data_container'>
+    <div className='real_estate_screen'>
+      <Container fluid>
+        <Row className='forms_container'>
+          <KitchenForm />
+        </Row>
+        {/* <Row className='room_data_container'>
         <KitchenData />
       </Row> */}
-    </Container>
+      </Container>
+    </div>
   );
 };
 
